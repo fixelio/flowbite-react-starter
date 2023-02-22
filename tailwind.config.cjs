5b751b90dc40c724a7e833e07cde493a3aaa686d
fixelio <julio.molina.m02@gmail.com>
2023-02-22 10:49:37 -0400

    Flowbite configuration added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
